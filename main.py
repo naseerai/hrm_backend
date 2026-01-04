58df16ffc4e8682498e4632f50276be6b5e8fb23
Dhanunjairam <dhanunjairam27@gmail.com>
2026-01-04 10:58:40 +0530

    face

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import logging
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 from src.career_routes.careers_routes import router as careers_router
 from src.calendar_routes.calendar_main_routes import router as calendar_router
+from src.leaves_routes.leaves_main_routes import router as leaves_router
+from src.attendance_routes.attendance_main_routes import router as attendance_router
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
@@ -26,4 +28,5 @@ app.include_router(login_router)
 app.include_router(user_router)
 app.include_router(careers_router)
 app.include_router(calendar_router)
-
+app.include_router(leaves_router)
+app.include_router(attendance_router)
